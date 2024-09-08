13cc9b0b145a07967b0af284943df01e5e815651
pegalink <85689068+pegalink@users.noreply.github.com>
2024-09-08 18:55:46 +0200

    createReply might not work

--- core.ts
+++ core.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 const WebSocketLib = require('ws');
 const EventEmitter = require('events');
-
-enum Dintents {
+let axiosInstanceAll: AxiosInstance;
+enum DiscordIntents {
   Guilds = 1 << 0,
   GuildMembers = 1 << 1,
   GuildModeration = 1 << 2,
@@ -13,7 +13,17 @@ enum Dintents {
   GuildVoiceStates = 1 << 7,
   GuildPresences = 1 << 8,
   GuildMessages = 1 << 9,
-  MessageContent = 1 << 15
+  GuildMessageReactions = 1 << 10,
+  GuildMessageTyping = 1 << 11,
+  DirectMessages = 1 << 12,
+  DirectMessageReactions = 1 << 13,
+  DirectMessageTyping = 1 << 14,
+  MessageContent = 1 << 15,
+  GuildScheduledEvents = 1 << 16,
+  AutoModerationConfiguration = 1 << 20,
+  AutoModerationExecution = 1 << 21,
+  GuildMessagePolls = 1 << 24,
+  DirectMessagePolls = 1 << 25
 }
 
 interface ClientOptions {
@@ -21,24 +31,136 @@ interface ClientOptions {
   token: string;
 }
 
-interface CreateMessageOptions {
-  content: string
-}
-
 enum DiscordEvents {
   Ready = 'READY',
   MessageCreate = 'MESSAGE_CREATE'
   // Add more events as needed
+}
+/*
+enum ChannelType {
+  GuildText = 0,
+  DM = 1,
+  GuildVoice = 2,
+  GroupDM = 3,
+  GuildCategory = 4,
+  GuildAnnoucement = 5,
+  AnnoucementThread = 10,
+  PublicThread = 11,
+  PrivateThread = 12,
+  GuildStage = 13,
+  GuildDirectory = 14,
+  GuildForum = 15
+}
+
+NOT YET KNOWN ON HOW IT WOULD BE IMPLEMENTED
+
+*/
+
+class Guild {
+  public id: string;
+  public name: string;
+  public client: any;
+  constructor(data, client) {
+    this.id = data.id;
+    this.name = data.name;
+    this.client = client;
+  }
+
+  async fetchChannel(id: String) {
+    return new Promise(async (resolve, reject) => {
+      const call = await axiosInstanceAll.get(`https://discord.com/api/v9/guilds/${id}/channels/${this.id}`);
+      if (call.status === 200) {
+        const d = call.data;
+        const channelData = {
+          id: d.id,
+          name: d.name,
+          type: d.type
+        }
+        resolve(new Channel(channelData, this));
+      } else {
+        reject(`An error happened while fetching channel: ${call.status}, ${call.statusText}`)
+      }
+    });
+  }
+}
+
+class Channel {
+  public id: string;
+  public name: string;
+  public type: any;
+  public client: any;
+  public guild: any
+  constructor(data, client) {
+    this.id = data.id;
+    this.name = data.name;
+    this.type = data.type;
+    this.client = client;
+    this.guild = data.guild
+  }
+
+    createMessage = async (content: string) => {
+    const params = {
+      "content": content
+    };
+    return new Promise(async (resolve, reject) => {
+      const call = await axiosInstanceAll.post(`https://discord.com/api/v9/channels/${this.id}/messages`, params);
+      if (call.status === 200) {
+        resolve(call);
+      } else {
+        reject(`An error happened while sending a message: ${call.status}, ${call.statusText}`)
+      }
+    });
+  }
+}
+
+class Message {
+  public id: string;
+  public content: string;
+  public channel: Channel;
+  public author: any;
+  constructor(data, client) {
+    this.id = data.id;
+    this.content = data.content;
+    this.channel = new Channel(data.channel, client);
+    this.author = data.author;
+  }
+
+  createMessage = async (content: string) => {
+    this.channel.createMessage(content)
+  }
+  createReply = async (content: string) => {
+    const call = await axiosInstanceAll.get(`https://discord.com/api/v9/channels/${this.id}`);
+    if (call.status !== 200) {
+      throw new Error(`An error occoured while replying: ${call.status}, ${call.statusText}.`)
+    }
+    const guildid = call.data.guild_id;
+    const params = {
+      "content": content,
+      "message_reference": {
+        "message_id": this.id,
+        "guild_id": guildid
+      }
+    };
+    return new Promise(async (resolve, reject) => {
+      const call = await axiosInstanceAll.post(`https://discord.com/api/v9/channels/${this.channel.id}/messages`, params);
+      if (call.status === 200) {
+        resolve(call);
+      } else {
+        reject(`An error happened while sending a message: ${call.status}, ${call.statusText}`)
+      }
+    });
+  }
 }
 
 class Client extends EventEmitter {
   private token: string;
   private intents: number[];
   private ws: any;
-  private heartbeatInterval: NodeJS.Timeout | null;
-  private heartbeatTimeout: NodeJS.Timeout | null;
-  private axiosInstance: AxiosInstance
+  public axiosInstance: AxiosInstance
   private lastsequence: Number
+  private websocketUrlForReconnect: string
+  private sessionId: any
+  private acknowledgedHeartbeat: boolean
 
   constructor(options: ClientOptions) {
     super();
@@ -66,12 +188,19 @@ class Client extends EventEmitter {
     }
   }
   async fetchGuild(id: string) {
-    const call = await this.axiosInstance.get(`https://discord.com/api/v9/guilds/${id}`);
-    if (call.status === 200) {
-      return call.data;
-    } else {
-      throw new Error(`Error happened while fetching guild: ${call.status}, ${call.statusText}`)
-    }
+    return new Promise(async (resolve, reject) => {
+      const call = await this.axiosInstance.get(`https://discord.com/api/v9/guilds/${id}`);
+      if (call.status === 200) {
+        const d = call.data;
+        const guildData = {
+          id: d.id,
+          name: d.name,
+        }
+        resolve(new Guild(guildData, this))
+      } else {
+        reject(`An error occoured while fetching a guild: ${call.status}, ${call.statusText}`);
+      }
+    });
   }
   async createMessage(channelid: string, message: string) {
     const params = {
@@ -93,16 +222,21 @@ class Client extends EventEmitter {
       const axiosstance = axios.create({
         headers: {
           'Authorization': `Bot ${this.token}`,
+          'Content-Type': 'application/json'
         },
       });
       this.axiosInstance = axiosstance;
+      axiosInstanceAll = axiosstance;
       this.ws = new WebSocketLib(wsUrl);
-      const identifyIntents = this.intents.reduce((acc, current) => acc + current, 0);
+      let mhm = 0;
+      this.intents.forEach(num => {
+        mhm += num
+      });
       const payload = {
         op: 2,
         d: {
           token: `Bot ${this.token}`,
-          intents: 33281,
+          intents: mhm,
           properties: {
             os: 'windows',
             browser: 'robo.js',
@@ -121,6 +255,7 @@ class Client extends EventEmitter {
         switch (op) {
           case 10: // Hello
             console.log(`Hello discord! Thanks, I now know that my heartbeat interval is ${data.d.heartbeat_interval}`)
+            this.acknowledgedHeartbeat = false
             this.heartbeat(35000)
             break;
           case 0: // Dispatch
@@ -131,19 +266,60 @@ class Client extends EventEmitter {
           }
             if (data.t === 'READY') {
               const userData = data.d;
+              this.sessionId = data.d.session_id;
+              this.websocketUrlForReconnect = data.d.resume_gateway_url;
               this.emit(DiscordEvents.Ready, userData.user);
             }
             if (data.t === 'MESSAGE_CREATE') {
-              this.emit(DiscordEvents.MessageCreate, d)
+              const togivesob = {
+                id: d.id,
+                content: d.content,
+                author: d.author,
+                channel: {
+                  id: d.channel_id,
+                  name: null,
+                  type: null
+                }
+              }
+              this.emit(DiscordEvents.MessageCreate, new Message(togivesob, this))
             }
             break;
           case 1: // Heartbeat requested from discord
             this.ws.send(JSON.stringify({op: 1, d: null}))
-            console.log('Sending additional heartbeat request, as discord requested')
+            this.acknowledgedHeartbeat = false
             break;
           case 11:
-            console.log('Recevied confirmation that discord indeed recevied heartbeat request. Continuing...')
+            this.acknowledgedHeartbeat = true
             break;
+          case 7:
+            this.ws.terminate();
+        this.ws = new WebSocketLib(`${this.websocketUrlForReconnect}/?v=10&encoding=json`);
+        const payload = {
+          op: 6,
+          d: {
+            token: `Bot ${this.token}`,
+            session_id: this.sessionId,
+            seq: this.lastsequence
+          }
+        };
+        this.ws.send(JSON.stringify(payload));
+          break;
+        case 9:
+          if (d === false) {
+            throw new Error('Discord terminated the websocket connection and is not allowing reconnection. Please run your code again\nNOTE: This is normal, and is expected, sometimes, websocket connections falls. Or discord denies it');
+          } else {
+        this.ws.destroy();
+        this.ws = new WebSocketLib(`${this.websocketUrlForReconnect}/?v=10&encoding=json`);
+        const payload = {
+          op: 6,
+          d: {
+            token: `Bot ${this.token}`,
+            session_id: this.sessionId,
+            seq: this.lastsequence
+          }
+        };
+        this.ws.send(JSON.stringify(payload));
+          }
           default:
             console.log(`Received unknown opcode ${data.op}`);
             break;
@@ -155,7 +331,18 @@ class Client extends EventEmitter {
       });
   
       this.ws.on('close', (code, reason) => {
-        console.log(`WebSocket connection closed with code ${code} and reason ${reason}. Reconnecting...`); // Reconnect on close
+        console.log(`WebSocket connection closed with code ${code} and reason ${reason}. Reconnecting...`);
+        this.ws.terminate()
+        this.ws = new WebSocketLib(`${this.websocketUrlForReconnect}/?v=10&encoding=json`);
+        const payload = {
+          op: 6,
+          d: {
+            token: `Bot ${this.token}`,
+            session_id: this.sessionId,
+            seq: this.lastsequence
+          }
+        };
+        this.ws.send(JSON.stringify(payload));
       });
     } catch (error) {
       console.error('Error connecting to WebSocket:', error);
@@ -163,4 +350,4 @@ class Client extends EventEmitter {
   }
 }
 
-module.exports = { Client, DiscordEvents, Dintents };
+module.exports = { Client, DiscordEvents, DiscordIntents };
